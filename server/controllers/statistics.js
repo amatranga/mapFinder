b3034b9a918c67430f4515f12378b462fe16037d
amatranga <amatranga892@gmail.com>
2017-08-12 20:52:13 -0700

    Implements statistics controller

--- server/controllers/statistics.js
+++ server/controllers/statistics.js
@@ -9,3 +9,26 @@ module.exports.getAll = (req, res) => {
       res.status(500).send(err);
     });
 };
+
+module.exports.getById = (req, res) => {
+  models.Statistic.where({ id: req.params.id }).fetch()
+    .then(stat => {
+      res.status(200).send(stat);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+};
+
+module.exports.update = (req, res) => {
+  models.Statistic.where({ id: req.params.id }).fetch()
+    .then(stat => {
+      stat.set({
+        value: req.body.value
+      }).save();
+      res.status(200).send(stat);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+};
