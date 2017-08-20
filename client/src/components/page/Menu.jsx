0586a3b591ee5d0464a5d919054523af13ddacef
amatranga <amatranga892@gmail.com>
2017-08-20 11:49:50 -0700

    page sub folder
    contains description, home, menu, and splash components

---
+++ client/src/components/page/Menu.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import MyMapButton from '../buttons/MyMapButton.jsx';
+import AchievementsButton from '../buttons/AchievementsButton.jsx';
+import StatisticsButton from '../buttons/StatisticsButton.jsx';
+import SettingsButton from '../buttons/SettingsButton.jsx';
+import LogoutButton from '../buttons/LogoutButton.jsx';
+
+const textStyle = {
+  padding: '1em',
+  fontWeight: 'bold',
+  fontSize: '1.60em',
+  color: '#222'
+};
+
+const Menu = () => {
+  return (
+    <div className="col">
+      <span className="text-center">
+        <a href="/myMap" style={textStyle}><p>My Map</p></a>
+        <a href="/achievements" style={textStyle}><p>Achievements</p></a>
+        <a href="/statistics" style={textStyle}><p>Statistics</p></a>
+        <a href="/settings" style={textStyle}><p>Settings</p></a>
+        <a href="/logout" style={textStyle}><p>Logout</p></a>
+      </span>
+    </div>
+  );
+}
+
+export default Menu;
---
+++ client/src/components/page/Splash.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Splash = () => {
+  return (
+    <div className="col text-center">
+      <h1> MapFinder </h1>
+      <p>Explore your world</p>
+    </div>
+  );
+}
+
+export default Splash;
